5b63d297a24e7f822a19ba90503f10eb460e2565
kerenmevorach <mevok873@newschool.edu>
2023-02-14 18:00:37 -0500

    added carousel and grid buttons

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,31 @@
-let aboutButton = document.getElementById('about');
-let closeButton = document.getElementById('close')
-let aboutPage = document.getElementById('about-page')
+let body = document.querySelector('body')
+
+let aboutButton = document.querySelector('#about');
+let closeButton = document.querySelector('#close');
+let aboutPage = document.querySelector('#about-page');
+
+let imageGrid = document.querySelector('#image-grid');
+let gridButton = document.querySelector('#grid-button');
+let carouselButton = document.querySelector('#carousel-button');
 
 aboutButton.onclick = function () {
     console.log("open about")
     aboutPage.style.display = 'block'
+  
 }
 
 closeButton.onclick = function () {
     console.log("close about")
     aboutPage.style.display = 'none'
+}
+
+
+carouselButton.onclick= function () {
+    console.log("carousel")
+    body.removeChild(imageGrid);
+}
+
+gridButton.onclick= function () {
+    console.log("grid")
+    body.appendChild(imageGrid);
 }
